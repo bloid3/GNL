8f7d9543fc8843dbb88e848eec6acfa65f111a64
bloid3 <pablo.pereira00@gmail.com>
2023-07-11 19:56:18 +0200

    1107

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,15 @@
 /*   By: papereir <papereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:50:35 by papereir          #+#    #+#             */
-/*   Updated: 2023/07/11 18:14:30 by papereir         ###   ########.fr       */
+/*   Updated: 2023/07/11 19:51:06 by papereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*tdod = NULL;
+	char		*goodline;
+	
+}
