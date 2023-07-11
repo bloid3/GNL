324996c837af7f670ff346642bcd43917f906681
bloid3 <pablo.pereira00@gmail.com>
2023-07-11 18:39:35 +0200

    1107

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,22 @@
 /*   By: papereir <papereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 16:21:14 by papereir          #+#    #+#             */
-/*   Updated: 2023/01/24 16:21:15 by papereir         ###   ########.fr       */
+/*   Updated: 2023/07/11 18:23:43 by papereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *str, int c);
+size_t	ft_strline(char *str);
+#endif
